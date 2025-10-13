a95454dfa2fb0887b8c02ab41b0e95d4cc2bcbcb
Siddiq <m4siddiq@gmail.com>
2025-10-13 17:24:17 +0100

    initial commit: Book List API

---
+++ src/models/bookModel.js
@@ -0,0 +1,7 @@
+//A simple booke model with an in-memory array; later we'll replace with a DB
+let books = [
+  { id: 1, title: "1984", author: "George Orwell", read: true },
+  { id: 2, title: "The Hobbit", author: "J.R.R. Tolkien", read: false },
+];
+
+module.exports = books;
