8a721a07c8e93d0068c1ba36366dcd6dd1658c49
ACACACanne <annechabab07@gmail.com>
2025-10-14 22:30:28 +0100

    project functionality set up

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const bookRoutes = require('./routes/bookRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api/books', bookRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Book List API running on port ${PORT}`);
+});
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
