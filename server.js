a37298f041d2aa6b55ce8d042068acb41bbd38eb
Siddiq <m4siddiq@gmail.com>
2025-10-16 10:13:33 +0100

    files fixed

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const bookRoutes = require('./routes/bookRoutes');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const bookRoutes = require("./src/routes/bookRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use('/api/books', bookRoutes);
+app.use("/api/books", bookRoutes);
 
 // Start server
 app.listen(PORT, () => {
@@ -19,6 +19,6 @@ app.listen(PORT, () => {
 });
 
 // Serve the index.html file
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
